9e36ace53078dd19d96314260a97a4b3cb331f35
Jack Yu <llacque@gmail.com>
2015-04-22 12:53:23 +0800

    create function to return power of 10 to bring a float to some decimal

--- climatools/muths.py
+++ climatools/muths.py
@@ -7,3 +7,14 @@ def round_to_1(number):
     This rounds a number to the first significant number
     '''
     return np.round(number, -int(np.floor(np.log10(number))))
+
+
+def pow_base10_for_decimal(number, decimal = 0):
+    '''
+    Returns the power of 10 NUMBER is required to
+    be multipliedy by to have a coefficient to DECIMAL
+    number of decimal places. e.g. NUMBER = 3 and decimal = 1
+    gives -1, because 3e-1 = .3, which has 1 decimal place.
+    '''
+    return - int(np.floor(np.log10(number)) + int(decimal))
+    
