af33745f976a7e9309fa32bba6b8497519b0d452
Jack Yu <llacque@gmail.com>
2015-06-02 09:14:56 +0800

    (units) convert level (interface) pressures from hybrid levels to mbars

--- climatools/units.py
+++ climatools/units.py
@@ -1,12 +1,17 @@
 
 
 
-def lev_hybrid2mbar(ds):
+def hybrid2mbar(ds, level_type = 'lev'):
     '''
     returns xray DataArray of levels in mbar
     INPUT:
     ds --- xray Dataset from .nc open_dataset()
+    level_type --- \'lev\' for layer(level) pressure
+                   \'ilev\' for level(interface) pressure
     '''
-    p = 1e-2 * (ds['hyam'] * ds['P0'] + ds['hybm'] * ds['PS'])
+    if level_type == 'lev':
+        p = 1e-2 * (ds['hyam'] * ds['P0'] + ds['hybm'] * ds['PS'])
+    elif level_type == 'ilev':
+        p = 1e-2 * (ds['hyai'] * ds['P0'] + ds['hybi'] * ds['PS'])
     p.attrs['units'] = 'mbar'
     return p[{'time': 0, 'lat': 0, 'lon': 0}]
