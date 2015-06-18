352e5b634b3bc24e14085b8a16013c8e10502031
Jack Yu <llacque@gmail.com>
2015-06-18 16:51:02 +0800

    (viz) organise matplotlib colormap names by type

--- climatools/viz.py
+++ climatools/viz.py
@@ -12,6 +12,31 @@ import climatools.misc as misc
 
 
 
+def matplotlib_colormap_names(kind = 'divergent'):
+    cmaps = dict([('Sequential', ['Blues', 'BuGn', 'BuPu',
+                                  'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
+                                  'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
+                                  'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
+                  ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool', 'copper',
+                                      'gist_heat', 'gray', 'hot', 'pink',
+                                      'spring', 'summer', 'winter']),
+                  ('Diverging',      ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
+                                      'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
+                                      'seismic']),
+                  ('Qualitative',    ['Accent', 'Dark2', 'Paired', 'Pastel1',
+                                      'Pastel2', 'Set1', 'Set2', 'Set3']),
+                  ('Miscellaneous',  ['gist_earth', 'terrain', 'ocean', 'gist_stern',
+                                      'brg', 'CMRmap', 'cubehelix',
+                                      'gnuplot', 'gnuplot2', 'gist_ncar',
+                                      'nipy_spectral', 'jet', 'rainbow',
+                                      'gist_rainbow', 'hsv', 'flag', 'prism'])])
+    
+    ccmaps = dict([('sequential', cmaps['Sequential'] + cmaps['Sequential (2)']),
+                   ('diverging', cmaps['Diverging']),
+                   ('qualitative', cmaps['Qualitative']),
+                   ('misc', cmaps['Miscellaneous'])])
+    return ccmaps[kind]
+    
 
 def symmetric_about_white_cmap_levels(rough_maxabs, Ncolours = 11):
     '''
