37600ba473d2099383ea93ddb45b4f499c60bf09
Jack Yu <llacque@gmail.com>
2015-07-06 14:56:13 +0800

    (data prep) pass attributes from datasets to datasets of their differences

--- climatools/prepdatasets.py
+++ climatools/prepdatasets.py
@@ -38,3 +38,34 @@ def take_difference_between_cases(datasets):
     
     return {x + ' - ' + y: datasets[x] - datasets[y]
             for x, y in diff_strs}
+
+
+
+def passon_attrs_casename(datasets, diff_datasets):
+    '''
+    Pass on attributes from DataArrays to differences between
+    DataArrays
+    INPUT:
+    datasets --- dictionary of datasets between which differences have been taken
+    diff_datasets --- dictionary of datasets of differences
+    OUTPUT:
+    datasets --- dictionary of datasets, each with a new attribute of case_name,
+                 its key in the dictionary
+    diff_datasets --- dictionary of datasets of differences, with attributes passed
+                      from original DATASETS. Each dataset also has a new attribute of case_name,
+                      its key in the dictionary
+    '''
+    interests = get_d3interests()
+
+    for ds_name, ds in diff_datasets.items():
+        ds.attrs['case_name'] = ds_name
+        for interest in interests:
+            ds[interest].attrs = dict(
+                datasets[random.choice(list(datasets.keys()))][interest].attrs)
+            ds[interest].attrs['case_name'] = ds_name
+            
+    for ds_name, ds in datasets.items():
+        for interest in interests:
+            ds[interest].attrs['case_name'] = ds_name
+            
+    return datasets, diff_datasets
