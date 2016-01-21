7b27ed9f1237ba75f2255287bb79b0960d62ef12
Jack Yu <llacque@gmail.com>
2016-01-21 15:14:40 +0800

    modal_aero_wateruptake. remove nstep (time step) from subroutine interface.

--- climatools/cesm_atm_aerosol/modal_aero_wateruptake.F90
+++ climatools/cesm_atm_aerosol/modal_aero_wateruptake.F90
@@ -2,7 +2,7 @@
                                                                                                                              
 !----------------------------------------------------------------------
 subroutine modal_aero_wateruptake_sub(                &
-     ncol, nstep,                        &
+     ncol,                         &
      iwaterup_flag, loffset,                    &
      h2o_mmr_flag,               &
      deltat, h2ommr, t, pmid, pdel, cldn,       &
@@ -47,7 +47,6 @@ subroutine modal_aero_wateruptake_sub(                &
   
   
   integer,  intent(in)  :: ncol               ! number of columns
-  integer,  intent(in)  :: nstep              ! time step
   integer,  intent(in)  :: iwaterup_flag       
   ! identifies call location from typhysbc (1,2)
   integer,  intent(in)  :: loffset            ! offset applied to modal aero "pointers"
