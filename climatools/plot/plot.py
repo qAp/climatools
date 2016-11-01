d57087475e1b53273e00b97d3f913296ddef6bb7
Jack Yu <llacque@gmail.com>
2016-11-01 14:08:46 +0800

    copy over xarray's plot.plot.line for plotting 1-d arrays
    As it is,
    
    `xr.DataArray.plot()`
    
    is the same as
    
    `xr.DataArray.climaviz.plot()`
    
    for 1-D arrays.  However, in the latter it is no longer
    checked if the index type is plottable.  For now,
    the function `_ensure_plottable` is not copied over
    from xarray.

--- climatools/plot/plot.py
+++ climatools/plot/plot.py
@@ -1,7 +1,7 @@
 
 
 
-
+import numpy as np
 
 import xarray as xr
 
@@ -11,6 +11,53 @@ class ClimavizArrayAccessor(object):
     def __init__(self, xarray_da):
         self._obj = xarray_da
         
-    def plot(self):
-        return 'Plot!!!!!'
+    def plot(self, *args, **kwargs):
+        """
+        Line plot of 1 dimensional DataArray index against values
+        
+        Wraps matplotlib.pyplot.plot
+        
+        Parameters
+        ----------
+        darray : DataArray
+        Must be 1 dimensional
+        ax : matplotlib axes, optional
+        If not passed, uses the current axis
+        *args, **kwargs : optional
+        Additional arguments to matplotlib.pyplot.plot
+        
+        """
+        import matplotlib.pyplot as plt
+
+        darray = self._obj
+        
+        ndims = len(darray.dims)
+        if ndims != 1:
+            raise ValueError('Line plots are for 1 dimensional DataArrays. '
+                             'Passed DataArray has {ndims} '
+                             'dimensions'.format(ndims=ndims))
+        
+        # Ensures consistency with .plot method
+        ax = kwargs.pop('ax', None)
+        
+        if ax is None:
+            ax = plt.gca()
+            
+        xlabel, x = list(darray.indexes.items())[0]
+            
+        #_ensure_plottable([x])
+        
+        primitive = ax.plot(x, darray, *args, **kwargs)
+        
+        ax.set_xlabel(xlabel)
+        ax.set_title(darray._title_for_slice())
+        
+        if darray.name is not None:
+            ax.set_ylabel(darray.name)
+
+        # Rotate dates on xlabels
+        if np.issubdtype(x.dtype, np.datetime64):
+            plt.gcf().autofmt_xdate()
+
+        return primitive
         
