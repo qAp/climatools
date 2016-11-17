1f33f9f8ee8d74edad3cfe71bb108837648ffda7
Jack Yu <llacque@gmail.com>
2016-11-17 11:56:11 +0800

    Create markdown for section title from string

--- climatools/html/html.py
+++ climatools/html/html.py
@@ -10,3 +10,9 @@ def getHTML_hrefanchor(s):
 def getHTML_idanchor(s):
     id = '_'.join(s.split())
     return '''<a id="{}"></a>'''.format(id)
+
+
+def getMarkdown_sectitle(s, n_hashtag=1):
+    return n_hashtag * '#' + ' ' + s
+
+    
