4b696159fa209e41eb2483827f6179fd96fd71ba
Jack Yu <llacque@gmail.com>
2016-11-17 11:55:16 +0800

    Create HTML id anchor from string

--- climatools/html/html.py
+++ climatools/html/html.py
@@ -5,3 +5,8 @@ def getHTML_hrefanchor(s):
     id = '_'.join(s.split())
     return '''<a href="#{}">{}</a>'''.format(id, s)
 
+
+
+def getHTML_idanchor(s):
+    id = '_'.join(s.split())
+    return '''<a id="{}"></a>'''.format(id)
