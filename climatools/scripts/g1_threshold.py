c7e4d2931e8e844332d49a737656d9da3e3fadad
Jack Yu <llacque@gmail.com>
2016-11-18 17:58:05 +0800

    Load and plot flux and cooling rate
    Examples usage
    
    ```
    import climatools.plot.plot
    from climatools.html.html import *
    from climatools.scripts.g1_threshold import *
    
    analysis = Analysis()
    
    crd = analysis.model('crd')
    crd.rundir = os.path.join('/nuwa_cluster/home/jackyu',
                              'radiation/crd/LW/examples',
                              'separate_g_groups',
                              'study__lblnew_g1_threshold',
                              'n2o',
                              'band07_wn_1215_1380',
                              'P_50_T_250',
                              '4.e-21',
                              'midlatitude_summer')
    crd.type_model = 'crd'
    crd.linestyle = '-'
    crd.load_data()
    
    clirad = analysis.model('clirad')
    clirad.rundir = os.path.join('/nuwa_cluster/home/jackyu/radiation',
                                 'clirad/LW/examples',
                                 '1_0912_mdc_Steve_CLIRAD_lw',
                                 'g1_threshold',
                                 'n2o',
                                 'band07_wn_1215_1380',
                                 'P_50_T_250',
                                 '4.e-21',
                                 'tmp')
    clirad.type_model = 'clirad'
    clirad.linestyle = '--'
    clirad.load_data()
    
    fig = analysis.fig_fluxcoolr('fig')
    fig.vars_plot['flux'] = ['flug', 'fnetg']
    fig.vars_plot['cooling rate'] = ['coolrg']
    fig.colors['flug'] = 'darkred'
    fig.colors['fnetg'] = 'lightcoral'
    fig.colors['coolrg'] = 'blue'
    fig.varlims_from_indexrange['linear'] = (50, 1050)
    fig.varlims_from_indexrange['log'] = (1e-4, 50)
    
    fig.plot(analysis)
    ```

---
+++ climatools/scripts/g1_threshold.py
@@ -0,0 +1,147 @@
+import os
+import itertools
+
+import pandas as pd
+import xarray as xr
+import matplotlib
+import matplotlib.pyplot as plt
+
+from ..html.html import *
+
+from IPython import display
+
+
+
+
+def load_dataset_crd(vartype, rundir):
+    if vartype == 'flux':
+        fname = 'output_fluxg.dat'
+        vdim = 'level'
+    elif vartype == 'cooling rate':
+        fname = 'output_coolrg.dat'
+        vdim = 'layer'
+
+    fpath = os.path.join(rundir, fname)
+    
+    df = pd.read_csv(fpath, sep=r'\s+')
+    df = df.set_index(['pressure', 'g'])
+    ds = xr.Dataset.from_dataframe(df)
+    ds.coords[vdim] = ('pressure', ds[vdim].isel(g=0))
+    return ds
+
+
+
+def load_dataset_clirad(vartype, rundir):
+    if vartype == 'flux':
+        fname = 'output_fluxg.dat'
+        vdim = 'level'
+    elif vartype == 'cooling rate':
+        fname = 'output_coolrg.dat'
+        vdim = 'layer'
+
+    fpath = os.path.join(rundir, fname)
+
+    df = pd.read_csv(fpath, sep=r'\s+')
+    df = df.set_index(['pressure', 'band', 'g'])
+    ds = xr.Dataset.from_dataframe(df)
+    ds.coords[vdim] = ('pressure', ds[vdim].isel(g=0, band=0))
+    ds = ds.sel(band=7)
+    del ds.coords['band']
+    return ds
+
+
+
+class Model(object):
+    def __init__(self, **kwargs):
+        self.rundir = kwargs.pop('rundir', None)
+        self.type_model = kwargs.pop('type_model', None)
+        self.linestyle = '-'
+        self.data = {}
+
+    def load_data(self):
+        if not (self.rundir and self.type_model):
+            raise ValueError("Cannot load data"
+                             " because no directory and model type info")
+        
+        if self.type_model == 'crd':
+            self.data['flux'] = load_dataset_crd(vartype='flux',
+                                                 rundir=self.rundir)
+            self.data['cooling rate'] = load_dataset_crd(
+                vartype='cooling rate',
+                rundir=self.rundir)
+        elif self.type_model == 'clirad':
+            self.data['flux'] = load_dataset_clirad(
+                vartype='flux', rundir=self.rundir)
+            self.data['cooling rate'] = load_dataset_clirad(
+                vartype='cooling rate', rundir=self.rundir)
+
+
+
+class Fig_FluxCoolr(object):
+    def __init__(self):
+        self.ggroups = [1, 2, 3, 4]
+        vartypes = ['flux', 'cooling rate']
+        yscales = ['linear', 'log']
+        self.names_ax = list(itertools.product(vartypes, yscales))
+        self.hreftext = 'Figure: flux, cooling rate. g-group {g}'
+        self.vartypes = vartypes
+        self.yscales = yscales
+        
+        self.vars_plot = {vartype: None for vartype in vartypes}
+        self.colors = {varname: None
+                       for varname in ['flug', 'fnetg', 'coolrg']}
+        self.varlims_from_indexrange = {yscale: None for yscale in yscales}
+
+    def plot(self, analysis):
+        for g in self.ggroups:
+            
+            s = self.hreftext.format(g=g)
+            html = getHTML_idanchor(s=s)
+            markdown = getMarkdown_sectitle(s=s)
+            display.display(display.HTML(html))
+            display.display(display.Markdown(markdown))
+            
+            fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(15, 11))
+            axs = axs.flatten()
+
+            for ax, (vartype, yscale) in zip(axs, self.names_ax):
+                for varname in self.vars_plot[vartype]:
+                    for modelname, model in analysis.models.items():
+                        model.data[vartype][varname]\
+                             .sel(g=g)\
+                             .climaviz\
+                             .plot(ax=ax,
+                                   linewidth=2, grid=True,
+                                   label=modelname.upper() + ' ' + varname,
+                                   color=self.colors[varname],
+                                   linestyle=model.linestyle,
+                                   index_on_yaxis=True,
+                                   yincrease=False, yscale=yscale,
+                                   varlim_from_indexrange=self.varlims_from_indexrange[yscale])
+                ax.set_xlabel(vartype)
+                        
+            plt.tight_layout()
+            display.display(plt.gcf())
+            plt.close()        
+
+
+
+class Analysis(object):
+    def __init__(self):
+        self.models = {}
+        self.figs = {}
+
+    def model(self, name, **kwargs):
+        if name not in self.models:
+            self.models[name] = Model(**kwargs)
+        return self.models[name]
+        
+    def fig_fluxcoolr(self, name, **kwargs):
+        if name not in self.figs:
+            self.figs[name] = Fig_FluxCoolr(**kwargs)
+        return self.figs[name]
+
+
+        
+
+
