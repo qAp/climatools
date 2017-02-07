420989a414b1631ca80ec9f662e422dc4b5ed927
Jack Yu <llacque@gmail.com>
2017-02-07 16:25:33 +0800

    Divide an interval into user-specified lengths
    Numpy's linspace function evenly divides an interval.
    This function divides an interval into lengths specified
    by the user.

--- climatools/muths.py
+++ climatools/muths.py
@@ -17,4 +17,34 @@ def pow_base10_for_decimal(number, decimal = 0):
     gives -1, because 3e-1 = .3, which has 1 decimal place.
     '''
     return - int(np.floor(np.log10(number)) + int(decimal))
+
+
+def linspace_by_intervals(vmin, vmax, intervals=None):
+    '''
+    Divide the interval [`vmin`, `vmax`] into `len(intervals)`
+    intervals.  Returns the (`intervals` + 1) values,
+    including `vmin` and `vmax` in an numpy array.  The length
+    of the intervals between the returned values are given by
+    `intervals` (these values are used relative to each other).
+    Note that if all the intervals are of the same length, it
+    is easier to use numpy.linspace.
+
+    Parameters
+    ----------
+    vmin: minimum value
+    vmax: maximum value
+    intervals: the lengths of the intervals to divide the range
+               [vmin, vmax]
+    spce: [numpy array]
+          np.array([v0, v1, ..., vn]), where v0 = vmin, and vn = vmax
+    '''
+    if intervals is None:
+        intervals = np.array([1])
+
+    dv_norm = intervals / intervals.sum()
+    spce = vmin + np.concatenate(([0], np.cumsum(dv_norm * (vmax - vmin))))
+    return spce
+
+        
+
     
