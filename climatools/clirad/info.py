c4ce93efaa26eb4248c65b40f440b25eda244075
Jack Yu <llacque@gmail.com>
2017-09-05 19:02:46 +0800

    Change band numbers to strings in CLIRAD-LW
    In the clirad info module, the keys for the dictionary
    containing the spectral bands' ranges have been
    changed from type integer to type string, because
    band 3 is subdivided into 'a', 'b' and 'c'.

--- climatools/clirad/info.py
+++ climatools/clirad/info.py
@@ -18,23 +18,25 @@ def wavenumber_bands(region = 'sw'):
         d[7] = [(4400, 8200),]
         d[8] = [(1000, 4400)]
     elif region == 'lw':
-        d[1] = [(0, 340),]          #  h2o
-        d[2] = [(340, 540),]        #  h2o
-        d[3] = [(540, 800),]        #  h2o,cont,co2
-        d[4] = [(800, 980),]        #  h2o,cont,  co2,f11,f12,f22
-        d[5] = [(980, 1100),]       #  h2o,cont,o3
+        d['1'] = [(0, 340),]          #  h2o
+        d['2'] = [(340, 540),]        #  h2o
+        d['3a'] = [(540, 620),]        #  h2o,cont,co2
+        d['3b'] = [(620, 720),]
+        d['3c'] = [(720, 800),]
+        d['4'] = [(800, 980),]        #  h2o,cont,  co2,f11,f12,f22
+        d['5'] = [(980, 1100),]       #  h2o,cont,o3
         #  co2,f11
-        d[6] = [(1100, 1215),]      #    h2o,cont
+        d['6'] = [(1100, 1215),]      #    h2o,cont
         #        c                              n2o,ch4,f12,f22
-        d[7] = [(1215, 1380),]      #  h2o,cont
+        d['7'] = [(1215, 1380),]      #  h2o,cont
         #        c                              n2o,ch4
-        d[8] = [(1380, 1900),]      #    h2o
-        d[9] = [(1900, 3000),]      #    h2o
+        d['8'] = [(1380, 1900),]      #    h2o
+        d['9'] = [(1900, 3000),]      #    h2o
         #        c
         #        c In addition, a narrow band in the 17 micrometer region (Band 10) is added
         #        c    to compute flux reduction due to n2o
         #        c
-        d[10] = [(540, 620)]       # h2o,cont,co2,n2o
+        d['10'] = [(540, 620)]       # h2o,cont,co2,n2o
     else:
         raise ValueError('region must be either sw for shortwave, or lw for longwave')
     return d
