bf6398375e18e84865e77d3199269f41134874f0
Jack Yu <llacque@gmail.com>
2017-11-28 14:51:44 +0800

    Create module for running overlap cases
    'overlap cases' uses versions of 'lblnew.f' that
    calculate radiation when one or more absorber
    may be present.  In these calculations, the
    k-dist method parameters are assumed to be hard-wired
    in lblnew.f; these would most likely be values that
    have been empirically determined to be optimal for
    absorbers in particular spectral bands.
    
    So, there are fewer input parameters to be specified, because
    parameters like `ng_refs`, `wgt` are already hard-wired in
    'lblnew.f'.

---
+++ climatools/lblnew/setup_overlap.py
@@ -0,0 +1,33 @@
+
+
+
+import os
+import pprint
+import subprocess
+import collections
+import shutil
+import time
+import itertools
+import re
+
+import climatools.clirad.info as cliradinfo
+
+
+
+
+
+
+
+
+def run_fortran():
+    pass
+
+
+
+def analyse_case():
+    pass
+
+
+
+def git_addcommit():
+    pass
