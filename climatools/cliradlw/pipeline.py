42da7bc402a2a60ee6b5ea9041af859b09d6d0cd
Jack Yu <llacque@gmail.com>
2018-03-13 16:17:05 +0800

    Run analysis notebook given input parameter values
    Created function runs the analysis after clirad-lw has
    been run.  It creates a directory for the analysis, copies
    the template notebook to the directory, creates a 'param.py'
    containing the path to the corresponding fortran run and
    the input parameter values, copies that to the directory,
    then it executes the notebook.

--- climatools/cliradlw/pipeline.py
+++ climatools/cliradlw/pipeline.py
@@ -82,3 +82,57 @@ def run_fortran(param=None, setup=None):
     proc = subprocess.Popen(['./cliradlw.exe'], stdout=subprocess.PIPE)
     pprint.pprint(param)
     return proc
+
+
+
+def run_analysis(param, setup=None):
+    '''
+    Execute the analysis notebook (i.e. plot
+    and tabulate results) for a case.
+
+    Paramaters
+    -----------
+    param: dict
+        Dictionary of input values.  The keys and values                 
+          are the names and values of the input parameters.
+    '''
+    dir_case = get_analysis_dir(param, setup=setup)
+    
+    try:
+        os.makedirs(dir_case)
+    except FileExistsError:
+        pprint.pprint(param)
+        print('This case already exists.')
+        raise
+        
+    try:
+        os.chdir(dir_case)
+        path_ipynb = setup.PATH_IPYNB
+        print(path_ipynb)
+        assert os.system('cp {} .'.format(path_ipynb)) == 0
+    except AssertionError:
+        pprint.pprint(param)
+        print('Problem copying Notebook template to analysis '
+              'directory for this case.')
+        raise
+
+    # Write .py file, used as input for analysis notebook
+    dir_fortran = get_fortran_dir(param, setup=setup)
+    print(dir_fortran)
+    lines=[]
+    lines.append("DIR_FORTRAN = '{}'".format(dir_fortran))
+    lines.append("PARAM = {}".format(param))
+    os.chdir(dir_case)
+    with open('param.py', encoding='utf-8', mode='w') as f:
+        f.write('\n'.join(lines))
+        
+    pprint.pprint(param)
+        
+    return subprocess.Popen(['jupyter', 'nbconvert', 
+                             '--execute',
+                             '--ExecutePreprocessor.timeout=None',
+                             '--to', 'notebook',
+                             '--inplace',
+                             setup.FNAME_IPYNB], 
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
