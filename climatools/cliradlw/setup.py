9cfc715b4053c5520c2f324cf54b3d2244ddd409
Jack Yu <llacque@gmail.com>
2018-04-20 14:56:58 +0800

    Change regex for parameter input
    The previous way of enterting input into the
    Fortran code failed when h2o concentration is
    implicitly 0.  Simple string.replace also replaces
    the variable name with a number, resulting in
    expressions like '0. = 0.', when it should be
    'wa(i, k) = 0.'.
    
    In this commit, different regex groups are used to
    search for where input are to be entered, and instead
    of using string.replace(), re.sub, with back reference
    to matched groups, is used to enter the desired value.

--- climatools/cliradlw/setup.py
+++ climatools/cliradlw/setup.py
@@ -3,6 +3,7 @@ import re
 import collections
 
 
+
 # Specify the directory in which CLIRAD-LW source code is kept.
 DIR_SRC = os.path.join('/chia_cluster/home/jackyu/radiation',
                        'clirad-lw/LW/lee_hitran2012_update')
@@ -70,7 +71,7 @@ def pattern_atmpro():
     used in lblnew.f.
     '''
     return '''
-    (atmosphere_profiles/(.*)75.pro)
+    (atmosphere_profiles/)([a-z]{3,3})
     '''
 
 
@@ -82,11 +83,11 @@ def pattern_conc(name=None):
     else provided.
     '''
     if name == 'h2o':
-        pattern = r'(\n \s+ wa\(i,k\) \s* = \s* (.*))'
+        pattern = r'(\n \s+ wa\(i,k\) \s* = \s* )(.*)'
     elif name == 'o3':
-        pattern = r'(\n \s+ oa\(i,k\) \s* = \s* (.*))'
+        pattern = r'(\n \s+ oa\(i,k\) \s* = \s* )(.*)'
     elif name in ('co2', 'n2o', 'ch4',):
-        pattern = r'(\n \s+ {name} \s* = \s* (.*))'.format(name=name)
+        pattern = r'(\n \s+ {name} \s* = \s* )(.*)'.format(name=name)
     return pattern
 
 
@@ -96,7 +97,7 @@ def pattern_assign(name=None):
     Returns regular expression for a Fortran 
     variable assignment.
     '''
-    pattern=r'(\n \s* {} \s* = \s* (.*) \s* \n)'.format(name)
+    pattern=r'(\n \s* {} \s* = \s* )(.*)'.format(name)
     return pattern
 
 
@@ -130,14 +131,14 @@ def enter_input_params(path_cliradlw, param=None):
                 molecule[name] = float(param['molecule'][name])
         else:
             molecule[name] = 0
-    
+
     for name, conc in molecule.items():
         d_in[name]['regex'] = pattern_conc(name=name)
         if conc in ('wa(i,k)', 'oa(i,k)'):
             d_in[name]['input_value'] = conc
         else:
             d_in[name]['input_value'] = str(float(conc))
-
+    
     # Spectral bands to compute
     bands = [1 if b in set(param['band']) else 0 for b in range(1, 11 + 1)]
     bands = [str(b) for b in bands]
@@ -153,15 +154,12 @@ def enter_input_params(path_cliradlw, param=None):
     d_in['tsfc']['regex'] = pattern_assign(name='tb\(1\)')
     d_in['tsfc']['input_value'] = str(param['tsfc'])
 
-
     with open(path_cliradlw, mode='r', encoding='utf-8') as f:
         code = f.read()
 
     for name, d in d_in.items():
         regex = re.compile(d['regex'], re.VERBOSE)
-        statement, value = regex.findall(code)[0]
-        input_statement = statement.replace(value, d['input_value'])
-        code = code.replace(statement, input_statement)
+        code = regex.sub(r'\g<1>' + d['input_value'], code)
 
     with open(path_cliradlw, mode='w', encoding='utf-8') as f:
         f.write(code)    
