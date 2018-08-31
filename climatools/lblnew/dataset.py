c939fb60057a123c99c8472d6193c35aa9cbe784
Jack Yu <llacque@gmail.com>
2018-08-31 15:01:10 +0800

    Get params for model atmosphere
    A function is created that will return the
    input parameter dictionaries from either
    lblnew-bestf or lblnew-overlap, depending
    on the spectral band.  These input parameter
    dictionaries together make a typical atmosphrere.

--- climatools/lblnew/dataset.py
+++ climatools/lblnew/dataset.py
@@ -20,39 +20,60 @@ importlib.reload(runrecord_overlap)
 
 
 
-param_bands = absorbers.nongreys_byband()
 
-# Search constraints
-nv, dv = 1000, .001
+def params_atm(atmpro=None):
+    '''
+    Search the run records and return 
+    the input parameter dictionary of each spectral 
+    band, together making up the model atmosphere.
 
-params = []
-for band, dmol in param_bands.items():
-    band = utils.mapband_new2old()[band]
+    Parameters
+    ----------
+    atmpro: string
+        Atmosphere profile. Can be 'mls', 'saw', or 'trp'.
+        If None, returns whatever profile is already 
+        in the run records.
+    params: list
+        List of input parameter dictionaries that,
+         together, make up the model/typical atmosphere.
+    '''
+    param_bands = absorbers.nongreys_byband()
+
+    # Search constraints
+    nv, dv = 1000, .001
+
+    params = []
+    for band, dmol in param_bands.items():
+        band = utils.mapband_new2old()[band]
     
-    if len(dmol.keys()) == 1:
-        # Search in lblnew-bestfit records
-        molecule, conc = [(molecule, conc)
-                          for molecule, conc in dmol.items()][0]
+        if len(dmol.keys()) == 1:
+            # Search in lblnew-bestfit records
+            molecule, conc = [(molecule, conc)
+                              for molecule, conc in dmol.items()][0]
+    
+            conc = None if conc == 'atmpro' else conc
+    
+            params.extend(
+                [param for param in runrecord_bestfit.params()
+                 if param['band'] == band
+                 if param['molecule'] == molecule
+                 if param['conc'] == conc])
+    
+        if len(dmol.keys()) > 1:
+            # Search in lblnew-overlap records
+            params.extend(
+                [param for param in runrecord_overlap.params()
+                 if param['band'] == band
+                 if param['molecule'] == dmol
+                 if param['nv'] == nv
+                 if param['dv'] == dv])
 
-        conc = None if conc == 'atmpro' else conc
+    if atmpro:
+        for param in params:
+            param['atmpro'] = atmpro
 
-        params.extend(
-            [param for param in runrecord_bestfit.params()
-             if param['band'] == band
-             if param['molecule'] == molecule
-             if param['conc'] == conc])
-
-    if len(dmol.keys()) > 1:
-        # Search in lblnew-overlap records
-        params.extend(
-            [param for param in runrecord_overlap.params()
-             if param['band'] == band
-             if param['molecule'] == dmol
-             if param['nv'] == nv
-             if param['dv'] == dv])
+    return params
 
 
-
-pprint.pprint(params)
 
 
