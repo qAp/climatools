09e48649459ddc831b8da3ede84f6704a945c874
Jack Yu <llacque@gmail.com>
2018-09-10 09:47:56 +0800

    Don't hide input cells in modul.  Do it in notebook.

--- climatools/cliradlw/results_entire_range_cliradsinglerun.py
+++ climatools/cliradlw/results_entire_range_cliradsinglerun.py
@@ -488,23 +488,11 @@ def script():
     
 
 
-script()  
 
 
-# In[ ]:
+if __name__ == '__main__':
+    script()  
 
 
-display.HTML('''<script>
-code_show=true; 
-function code_toggle() {
- if (code_show){
- $('div.input').hide();
- } else {
- $('div.input').show();
- }
- code_show = !code_show
-} 
-$( document ).ready(code_toggle);
-</script>
-<form action="javascript:code_toggle()"><input type="submit" value="Click here to toggle on/off the raw code."></form>''')
+
 
