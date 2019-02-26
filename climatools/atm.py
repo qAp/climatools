678cd9b9152db6d2160114629cf1199c75d2414d
Jack Yu <llacque@gmail.com>
2019-02-26 15:59:24 +0800

    Create AtmComposition class.
    And let the atmosphere composition containing only
    the non-grey absorbers that we used as a test case to
    be an example of an atmosphere composition which
    can be loaded into an instance of this class.

--- climatools/atm.py
+++ climatools/atm.py
@@ -1,5 +1,8 @@
 
 
+from .cliradlw.utils import *
+from .parameters import *
+
 
 
 def greys_byband():
@@ -17,59 +20,62 @@ def greys_byband():
 
 
 
-def nongreys_byband():
-    '''
-    These are the concentrations of non-grey 
-    absorbers in each spectral band in 
-    an atmosphere commonly used as reference.
+class AtmComposition():
+    def __init__(self, gasinbands, gasconcs):
+        self.gasinbands = gasinbands
+        self.gasconcs = gasconcs
 
-    The absorption associated with each 
-    (band, gas) pair here is computed using 
-    the k-distrbution method.
-    '''
-    return {1: {'h2o': 'atmpro'},
-            2: {'h2o': 'atmpro'}, 
-            3: {'co2': 0.0004, 'h2o': 'atmpro', 'n2o': 3.2e-07},
-            4: {'co2': 0.0004, 'h2o': 'atmpro'},
-            5: {'co2': 0.0004, 'h2o': 'atmpro'},
-            6: {'co2': 0.0004, 'h2o': 'atmpro'},
-            7: {'co2': 0.0004, 'h2o': 'atmpro', 'o3': 'atmpro'},
-            8: {'h2o': 'atmpro'},
-            9: {'ch4': 1.8e-06, 'h2o': 'atmpro', 'n2o': 3.2e-07},
-            10: {'h2o': 'atmpro'},
-            11: {'co2': 0.0004, 'h2o': 'atmpro'}}
+    def to_cliradparam(self, **kwargs):
+        '''
+        Return clirad input parameter dictionary.
+        '''
+        band = list(self.gasinbands.keys())
+        molecule = self.gasconcs
+        return CliradnewLWParam(band=band, molecule=molecule, **kwargs)
 
-
-
-class ATMCompNongreys():
-    def __init__(self):
-        self.comp = nongreys_byband()
-
-    def get_param_dict(self, band=None, molecule=None):
-        if not band and not molecule:
-            bands = list(self.comp.keys())
-            molecule = {mol: conc for band, molconc in self.comp.items()
-                        for mol, conc in molconc.items()}
-            return {'band': bands, 'molecule': molecule}
-        
-        if not molecule and band:
-            return {'band': [band], 'molecule': self.comp[band]}
-        
-        if molecule and not band:
-            d = {}
-            d['molecule'] = {molecule: conc
-                             for band, molconc in self.comp.items()
-                             for mol, conc in molconc.items() if mol == molecule}
-            d['band'] = [band for band, molconc in self.comp.items()
-                         if molecule in molconc]
-            return d
-        
-        if molecule and band:
-            if molecule not in self.comp[band]:
-                print(f'There is no {molecule} in band {band}.')
-                return None
+    def to_lblnewparam(self, bestfitonly=False, **kwargs):
+        params = []
+        for b, gs in self.gasinbands.items():
+            band = mapband_new2old()[b] # lblnew has a different set of spectral band labels.
+            if len(gs) == 1:
+                mol = gs[0]
+                conc = None if self.gasconcs[mol] else self.gasconcs[mol]
+                params.append(
+                    LBLnewBestfitParam(band=band, molecule=mol, conc=conc, **kwargs))
             else:
-                return {'band': [band],
-                        'molecule': {mol: conc
-                                     for mol, conc in self.comp[band].items()
-                                     if mol == molecule}}
+                params.append(LBLnewOverlapParam(band=[band], molecule=gs, **kwargs))
+        return params
+        
+    @classmethod
+    def cliradlw_nongreys(cls):
+        '''
+        Returns the composition for an atmosphere that contains
+        only the 'non-grey' absorbers.  This was used as the 'overall'
+        test for the new k-distribution method
+        '''
+        gasinbands = {1: ['h2o'],
+                      2: ['h2o'],
+                      3: ['h2o', 'co2', 'n2o'],
+                      4: ['h2o', 'co2'],
+                      5: ['h2o', 'co2'],
+                      6: ['h2o', 'co2'],
+                      7: ['h2o', 'co2', 'o3'],
+                      8: ['h2o'],
+                      9: ['h2o', 'n2o', 'ch4'],
+                      10: ['h2o'],
+                      11: ['h2o', 'co2']}
+        gasconcs = {'h2o': 'atmpro',
+                    'co2': 400e-6,
+                    'o3': 'atmpro',
+                    'n2o': 3.2e-7,
+                    'ch4': 1.8e-6}
+        return cls(gasinbands, gasconcs)
+
+    def __repr__(self):
+        gasinbands = 'Gases in each band:\n' + str(self.gasinbands)
+        gasconcs = 'Gas concentrations:\n' + str(self.gasconcs)
+        return f'{self.__class__}\n{gasinbands}\n{gasconcs}'
+        
+    
+
+
