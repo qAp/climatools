4e3faa399b2f9cb2356f26cb2c4a6bffec92c193
Jack Yu <llacque@gmail.com>
2019-04-10 17:12:58 +0800

    Separate showing plots and updating hrefs
    You can choose to show the plots and tables and not
    update the table with a link to the plots and tables
    of the case.  Showing the plots and updating the links' table
    is done case-by-case.

--- climatools/cliradlw/cliradlw_latest.py
+++ climatools/cliradlw/cliradlw_latest.py
@@ -8,12 +8,7 @@ from ..plot.plot import *
 
 
 
-def markdown_anchor(dlbl=None, dcli=None):
-    s_band = f'''{dlbl.param['band']}/{dcli.param['band']}'''
-    s_conc = f'''{dcli.param['molecule']}'''
-    s_atmpro = f'''{dcli.param['atmpro']}'''
-    s = f'#### {s_band} &nbsp;&nbsp; {s_conc} &nbsp;&nbsp; {s_atmpro}'
-    display.display(display.Markdown(s))
+
 
 
     
@@ -57,23 +52,56 @@ def pltdata_diffcool(dlbl=None, dcli=None):
 
 
 
-def show_results(dlbl=None, dcli=None):
-    display.display(display.Markdown('-----------------------------'))
-    markdown_anchor(dlbl=dlbl, dcli=dcli)
-
-    # Parameters
+def show_parameters(dlbl=None, dcli=None):
     gs = dlbl.param['molecule'].keys() if isinstance(dlbl.param['molecule'], dict) else [dlbl.param['molecule']]
     ps_cliradfit = {f"({g}, {dlbl.param['band']})": kdist_params(molecule=g, band=dlbl.param['band']) for g in gs}
     fitparams = pd.DataFrame({molgas: pd.Series(p) for molgas, p in ps_cliradfit.items()})
     runparams = pd.concat([pd.Series(dcli.param), pd.Series(dlbl.param)], axis=1, sort=True, keys=['CLIRAD', 'LBLNEW'])
-    display.display(pd.concat([runparams, fitparams], axis=1, sort=True, keys=['Run parameters', 'Fit parameters']).fillna('-'))
+    display.display(pd.concat([runparams, fitparams], axis=1, sort=True, keys=['Run parameters', 'Fit parameters']).fillna('-'))    
 
-    # Plot cooling rate profiles
+
+
+def show_cools(dlbl=None, dcli=None):
     plotdata = pltdata_cool(dlbl=dlbl, dcli=dcli)
     diffdata = pltdata_diffcool(dlbl=dlbl, dcli=dcli)
     fig_liny = plt_vert_profile_bokeh(pltdata=plotdata, y_axis_type='linear', prange=(50, 1050))
     fig_logy = plt_vert_profile_bokeh(pltdata=plotdata, y_axis_type='log', prange=(.01, 200))
     fig_diff = plt_vert_profile_bokeh(pltdata=diffdata, y_axis_type='log', prange=(.01, 200))
-    show(gridplot(fig_liny, fig_logy, fig_diff, ncols=3, plot_height=500))
+    show(gridplot(fig_liny, fig_logy, fig_diff, ncols=3, plot_height=500))    
     
+
+
+def show_title(dlbl=None, dcli=None, add_idanchor=False):
+    band, molecule, atmpro = [dcli.param.get(p) for p in ('band', 'molecule', 'atmpro')]
+    title = f'{band} {molecule} {atmpro}'
+    if add_idanchor:
+        idanchor = ''.join(list(np.random.choice(range(9), 11).astype(str)))
+        html_idanchor = f'''<a id="{idanchor}"></a>'''
+        display.display(display.Markdown(html_idanchor))
+        display.display(display.Markdown(title))
+        return idanchor
+    else:
+        display.display(display.Markdown(title))
+
+
+
+def show_href(idanchor=None, df=None, dlbl=None, dcli=None):
+    band, molecule, atmpro = [dcli.param.get(p) for p in ('band', 'molecule', 'atmpro')]
+    band_lblnew = '[' + ','.join(mapband_new2old()[b] for b in band) + ']'
+    html_hrefanchor = f'''<a href="#{idanchor}">{atmpro}</a>'''
+    try:
+        df.loc[f"{molecule}", (f"{band_lblnew}", f"{band}")] = df.loc[f"{molecule}", (f"{band_lblnew}", f"{band}")] + (' ' + html_hrefanchor)
+    except KeyError:
+        df.loc[f"{molecule}", (f"{band_lblnew}", f"{band}")] = html_hrefanchor
+
+
+
+def show_results(dlbl=None, dcli=None, add_idanchor=None):
+    display.display(display.Markdown('-----------------------------'))
+    ida = show_title(dlbl=dlbl, dcli=dcli, add_idanchor=add_idanchor)
+    # Parameters
+    show_parameters(dlbl=dlbl, dcli=dcli)
+    # Plot cooling rate profiles
+    show_cools(dlbl=dlbl, dcli=dcli)
     display.display(display.Markdown('--------------------------'))
+    return ida
